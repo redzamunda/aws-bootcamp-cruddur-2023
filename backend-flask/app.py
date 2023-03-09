88ef659d4c6fff6217866af9832e5f53df0f9a07
redzamunda Ejike <genevievejike256@gmail.com>
2023-03-09 00:26:07 +0000

    added logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,23 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#X-ray -----
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#cloudwatch logs...
+import watchtower
+import logging
+from time import strftime
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
+
+
 # honeycomb OTEL...
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -27,10 +44,19 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+#X-ray -------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# Show this in the logs within the backend-flask app (STDOUT)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+#X-ray -------
 app = Flask(__name__)
+XRayMiddleware(app, xray_recorder)
+
+
+
 # HONEYCOMB
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
@@ -47,6 +73,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -85,7 +117,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(Logger: LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
